0246ea5de300c559707d82f053943b9e46c628c3
Ian Duncan <janskykd@gmail.com>
2020-08-01 17:12:07 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as readme_file:
+    long_description = readme_file.read()
+
+setuptools.setup(
+    name="ghupdatesecret",
+    version="0.0.1",
+    author="Ian Duncan",
+    author_email="janskykd@gmail.com",
+    description="A command-line utility to update GitHub repository secrets.",
+    long_description=long_description,
+    url="https://github.com/jansky/ghupdatesecret",
+    packages=['ghupdatesecret'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Version Control",
+        "Topic :: Software Development :: Version Control :: Git",
+    ],
+    install_requires=['requests>=2.24.0', 'pynacl>=1.4.0'],
+    entry_points={
+        'console_scripts':
+        ['ghupdatesecret=ghupdatesecret.ghupdatesecret:main']
+    },
+    python_requires='>=3.6',
+)
